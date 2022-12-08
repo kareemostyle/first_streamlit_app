51d12b1b2344829145762fc901fc5609f325c88f
Kareem Taleb <43004711+kareemostyle@users.noreply.github.com>
2022-12-08 16:30:24 -0500

    Added function to prevent code repetition.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#Create the function to encapsulate repeated code
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #Fruitvice API display
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -31,9 +37,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    function_output = get_fruityvice_data(fruit_choice_
+    streamlit.dataframe(function_output)
 
 except URLError as e:
   streamlit.error()
