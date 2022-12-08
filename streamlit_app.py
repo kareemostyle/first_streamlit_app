113cb0e6b3ae3922060abcbb31ccf3a466f2e1fb
Kareem Taleb <43004711+kareemostyle@users.noreply.github.com>
2022-12-08 14:55:06 -0500

    Update streamlit_app.py
    New code for Fruitvice API display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Snowflake Trial')
 
@@ -20,3 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#Fruitvice API display
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
